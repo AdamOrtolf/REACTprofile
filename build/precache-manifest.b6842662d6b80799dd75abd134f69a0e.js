40e610bbe18a28a0bb6daa7ba0c9f11746626dc7
Tao <tao8261@aol.com>
2020-06-02 16:41:05 -0600

    deploying to github pages

---
+++ build/precache-manifest.b6842662d6b80799dd75abd134f69a0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "effcb6e7054e6d4762cc9d18054dd07a",
+    "url": "/REACTprofile/index.html"
+  },
+  {
+    "revision": "7552b737290218d8c274",
+    "url": "/REACTprofile/static/css/main.e92333eb.chunk.css"
+  },
+  {
+    "revision": "099230975e37baadc835",
+    "url": "/REACTprofile/static/js/2.5fcece27.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/REACTprofile/static/js/2.5fcece27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7552b737290218d8c274",
+    "url": "/REACTprofile/static/js/main.736a7a43.chunk.js"
+  },
+  {
+    "revision": "bc5fd895bc6b7e1536ed",
+    "url": "/REACTprofile/static/js/runtime-main.02e7890a.js"
+  }
+]);
